150bbbca024c4c0518db2fa7529088965dc5db6e
Marcos <marcos.castro@liferay.com>
2016-04-08 02:27:24 +0200

    Changes to have always vertical-dockbar visible

--- dockbar-vertical-theme/docroot/_diffs/js/main.js
+++ dockbar-vertical-theme/docroot/_diffs/js/main.js
@@ -13,32 +13,5 @@ AUI().ready(
 		if (signIn && signIn.getData('redirect') !== 'true') {
 			signIn.plug(Liferay.SignInModal);
 		}
-
-		// Dockbar vertical JS, if dockbar is present
-
-		var portletDockbar = A.one('#_145_dockbar');
-
-		if (portletDockbar) {
-			var body = A.one('.aui body');
-			body.append('<div class="icon-toggle-dockbar vertical-dockbar-close"><i class="icon-cog"></i></div>');
-			body.append('<div class="layer-mobile visible-phone vertical-dockbar-close"></div>');
-
-			var toggleDockbar = A.one('.icon-toggle-dockbar');
-			var toggleDockbarClose = A.all('.vertical-dockbar-close');
-			var toggleDockbarIcon = A.one('.icon-toggle-dockbar .icon-cog');
-
-			if (toggleDockbar) {
-				toggleDockbarClose.on(
-					'click',
-					function() {
-						portletDockbar.toggleClass('over');
-						toggleDockbar.toggleClass('over');
-						toggleDockbarIcon.toggleClass('icon-remove');
-						toggleDockbarIcon.toggleClass('icon-cog');
-						body.toggleClass('lfr-has-dockbar-vertical');
-					}
-				);
-			}
-		};
 	}
 );
